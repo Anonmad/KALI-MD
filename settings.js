e774cfd21dfaa910bf4242eae7265f238705afd5
ANONMAD NETWORK <anonymousmad74@gmail.com>
2026-02-26 02:21:28 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Kali Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Kali Bot",
+  botOwner: 'Kali-oS', // Your name
+  ownerNumber: 255619615065', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
